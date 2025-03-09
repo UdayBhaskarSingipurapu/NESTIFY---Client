ebef1e1f98b861db1ea66a7b308106a29cc30521
SeemaJabeen <22501a05g6@pvpsit.ac.in>
2025-03-09 21:19:58 +0530

    fix:search bar,added logo,menu

--- src/components/User/sidebar/Sidebar.jsx
+++ src/components/User/sidebar/Sidebar.jsx
@@ -1,15 +1,13 @@
 import React, { useContext, useState,useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { LayoutDashboard, User, Settings, LogOut, Home, Menu } from "lucide-react";
+import { LayoutDashboard, User, Settings, LogOut, Home, Menu,X } from "lucide-react";
 import { userLoginContext } from "../../../contexts/userLoginContext";
-
+import Slogo from "../../../assets/Slogo.png"
 
 const Sidebar = () => {
   const { logout } = useContext(userLoginContext);
   const navigate = useNavigate();
   const [isSidebarVisible, setSidebarVisible] = useState(false);
-
-  
 
   const handleLogout = () => {
     logout();
@@ -22,12 +20,16 @@ const Sidebar = () => {
 
   return (
     <>
-      {/* Menu Button for Medium Screens */}
+
       <button
         onClick={toggleSidebar}
-        className="fixed top-4 left-4 z-50 p-2 bg-gray-900 text-white rounded-md md:hidden "
+        className="fixed z-50 p-2 bg-gray-900 text-white rounded-md md:hidden  "
       >
-        <Menu className="h-6 w-6" />
+       {isSidebarVisible ? (
+          <X className="h-6 w-6" />
+        ) : (
+          <Menu className="h-6 w-6" /> 
+        )}
       </button>
 
       {/* Sidebar */}
@@ -36,8 +38,10 @@ const Sidebar = () => {
           isSidebarVisible ? "translate-x-0" : "-translate-x-full"
         } md:translate-x-0`}
       >
-        <div className="text-lg font-bold mb-8">Logo</div>
-        <nav className="flex flex-col space-y-4">
+      <img src={Slogo} alt="logo" className="w-15 h-15 mb-0 mt-10 "/>
+
+       
+        <nav className="flex flex-col space-y-4 mt-3">
           <Link
             to="/student-home"
             className="nav-link flex items-center gap-2 p-2 hover:bg-gray-300 hover:text-black rounded-md"
